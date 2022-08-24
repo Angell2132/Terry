3e50534921df750acf1db4f1c31631107010a471
Angell2132 <111932348+Angell2132@users.noreply.github.com>
2022-08-23 19:25:58 -0500

    Add files via upload

---
+++ Pseint resta.py
@@ -0,0 +1,17 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	num1 = int()
+	num2 = int()
+	op = int()
+	print "dime el primer numero a restar"
+	num1 = int(raw_input())
+	print "dime el segundo numero a restar"
+	num2 = int(raw_input())
+	op = num1-num2
+	print "el resultado de la resta es",op
+
